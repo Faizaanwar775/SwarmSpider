bf4736dca65aa8e842d4e5009c681296402c4aa5
Faiza Anwar <153000753+Faizaanwar775@users.noreply.github.com>
2026-08-05 21:19:00 +0500

    Add files via upload

--- cli/main.py
+++ cli/main.py
@@ -1,1 +1,19 @@
+import asyncio
 
+from db.init_db import init_db
+
+from pipeline.pipeline import Pipeline
+
+
+async def main():
+
+    await init_db()
+
+    pipeline = Pipeline()
+
+    await pipeline.run()
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
